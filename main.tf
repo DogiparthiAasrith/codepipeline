ae8beab4346cb29fa3860c26fc1a23a3ab376988
DogiparthiAasrith <aasrithdogiparthi03@gmail.com>
2026-03-21 23:26:48 +0530

    Uploaded

--- main.tf
+++ main.tf
@@ -80,6 +80,28 @@ resource "aws_dynamodb_table" "demo_table" {
   }
 }
 
+# Create second DynamoDB table
+resource "aws_dynamodb_table" "users_table" {
+  name           = "terraform-users-table-${data.aws_caller_identity.current.account_id}"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+  range_key      = "timestamp"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "N"
+  }
+
+  tags = {
+    Name = "Users DynamoDB Table"
+  }
+}
+
 # Get current AWS account ID
 data "aws_caller_identity" "current" {}
 
