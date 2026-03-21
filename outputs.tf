10901f1c36d8668e42ca7a2f9b56622768409d9e
DogiparthiAasrith <aasrithdogiparthi03@gmail.com>
2026-03-21 16:42:43 +0530

    Uploaded

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "bucket_name" {
   description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.app_bucket.id
+  value       = data.aws_s3_bucket.app_bucket.id
 }
 
 output "bucket_arn" {
   description = "ARN of the S3 bucket"
-  value       = aws_s3_bucket.app_bucket.arn
+  value       = data.aws_s3_bucket.app_bucket.arn
 }
 
 output "bucket_region" {
   description = "Region of the S3 bucket"
-  value       = aws_s3_bucket.app_bucket.region
+  value       = data.aws_s3_bucket.app_bucket.region
 }
 
 output "account_id" {
